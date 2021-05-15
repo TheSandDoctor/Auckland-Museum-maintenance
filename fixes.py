9e8f1e92c16464aa659cb17a903f8a724ffdca76
TheSandDoctor <majorjohn1@mail.com>
2021-05-14 20:44:04 -0700

    Fixes

--- fixes.py
+++ fixes.py
@@ -13,20 +13,24 @@ def search_in(page):
     else:
         return None
 
-
 search = r"\[(?:https?:\/\/api\.aucklandmuseum\.com\/id\/)([^\/]+)\/([^\/]+)\/(\d+) Object record\]\<br\>"
-
 site = pywikibot.Site('commons','commons')
 
-cat = pywikibot.Category(site, "Category:Images from Auckland Museum")
+def main():
+    cat = pywikibot.Category(site, "Category:Images from Auckland Museum")
+    for page in pagegenerators.CategorizedPageGenerator(cat):
+        wikicode = mwparserfromhell.parse(page.text)
+        for template in wikicode.filter_templates():
+            if template.name == "Images from Auckland Museum":
+                print("Already done")
+                continue
+        search_result = search_in(page)
+        if search_result:
+            page.text = search_result
+            page.save(summary=u"Adding [[Template:Images from Auckland Museum]] and updating [[Template:Photograph]] source ([[Commons:Bots/Requests/TheSandBot 4|BRFA]])", minor=True, botflag=True, force=True)
 
-for page in pagegenerators.CategorizedPageGenerator(cat):
-    wikicode = mwparserfromhell.parse(page.text)
-    for template in wikicode.filter_templates():
-        if template.name == "Images from Auckland Museum":
-            print("Already done")
-            continue
-    page.text = search_in(page)
-    if page.text is not None:
-        page.save(summary=u"Adding [[Template:Images from Auckland Museum]] and updating [[Template:Photograph]] source ([[Commons:Bots/Requests/TheSandBot 4|BRFA]])", minor=True, botflag=True, force=True)
-        print("Saved")
+if __name__ == '__main__':
+    try:
+        main()
+    except KeyboardInterrupt:
+        pass
